b46beea10083f64461255d839579e3ee259e94ee
harenlin <allen880530@gmail.com>
2024-02-17 14:42:37 -0800

    gg=G

--- ex6/Twenty.java
+++ ex6/Twenty.java
@@ -1,13 +1,13 @@
 /* 
-def load_plugins():
-    config = configparser.ConfigParser()
-    config.read("config.ini")
-    words_plugin = config.get("Plugins", "words")
-    frequencies_plugin = config.get("Plugins", "frequencies")
-    global tfwords, tffreqs
-    tfwords = importlib.machinery.SourcelessFileLoader('tfwords', words_plugin).load_module()
-    tffreqs = importlib.machinery.SourcelessFileLoader('tffreqs', frequencies_plugin).load_module()
-*/
+   def load_plugins():
+   config = configparser.ConfigParser()
+   config.read("config.ini")
+   words_plugin = config.get("Plugins", "words")
+   frequencies_plugin = config.get("Plugins", "frequencies")
+   global tfwords, tffreqs
+   tfwords = importlib.machinery.SourcelessFileLoader('tfwords', words_plugin).load_module()
+   tffreqs = importlib.machinery.SourcelessFileLoader('tffreqs', frequencies_plugin).load_module()
+ */
 
 /* [SPEC]
  * Provide 2 plugins for extracting words: 
@@ -35,18 +35,18 @@ public class Twenty {
 
 	private static ITFWords Extractor;
 	private static ITFCount Counter;
-	
+
 	public static void load_plugins() {
-    	Properties propt = new Properties();
+		Properties propt = new Properties();
 		try {
 			propt.load(new FileInputStream(CONFIG_PATH));
 		} catch(Exception e){
 			e.printStackTrace();
 		} 
-		
+
 		String words_class_name = propt.getProperty("words");
-        String words_Jar_file_name = "plugins/" + words_class_name + ".jar";
-        URL wordsJarURL = null;
+		String words_Jar_file_name = "plugins/" + words_class_name + ".jar";
+		URL wordsJarURL = null;
 		try {
 			wordsJarURL = new File(words_Jar_file_name).toURI().toURL();
 		} catch(Exception e){
@@ -55,8 +55,8 @@ public class Twenty {
 		// System.out.println("---" + words_class_name + " " + words_Jar_file_name);
 
 		String count_class_name = propt.getProperty("count");
-        String count_Jar_file_name = "plugins/" + count_class_name + ".jar";
-        URL countJarURL = null;
+		String count_Jar_file_name = "plugins/" + count_class_name + ".jar";
+		URL countJarURL = null;
 		try {
 			countJarURL = new File(count_Jar_file_name).toURI().toURL();
 		} catch(Exception e){
@@ -69,7 +69,7 @@ public class Twenty {
 
 		// System.out.println(wordsJarURL);
 		// System.out.println(countJarURL);
-		
+
 		try {
 			Extractor = (ITFWords) classLoader1.loadClass(words_class_name).getDeclaredConstructor().newInstance();     
 			Counter = (ITFCount) classLoader2.loadClass(count_class_name).getDeclaredConstructor().newInstance();
@@ -79,17 +79,17 @@ public class Twenty {
 
 	}
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 		if( args.length < 1 ){
 			System.err.println("Please specify your /path/to/file.");
 			System.exit(1);
 		} 
-		
+
 		load_plugins();
 		List<Map. Entry<String, Integer>> word_freqs = Counter.count(Extractor.extract(args[0]));
 		for(Map.Entry<String, Integer> entry : word_freqs) {
-            System.out.println(entry.getKey() + "  -  " + entry.getValue());
-        }
-    }
+			System.out.println(entry.getKey() + "  -  " + entry.getValue());
+		}
+	}
 }
 
--- ex6/plugins-src/framework/ITFWords.java
+++ ex6/plugins-src/framework/ITFWords.java
@@ -1,6 +1,5 @@
 import java.util.List;
-import java.util.Map;
 
 public interface ITFWords {
-    List<String> extract(String path_to_file);
+	List<String> extract(String path_to_file);
 }
