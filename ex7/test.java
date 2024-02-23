de8ac88971fcd06e3ad5dc66329922f218d7536f
harenlin <allen880530@gmail.com>
2024-02-23 14:53:22 -0800

    update Streams

--- ex7/test.java
+++ ex7/test.java
@@ -10,46 +10,46 @@ import java.util.stream.StreamSupport;
 public class test {
 
 	public static class CharactersSpliterator implements Spliterator<Character> {
-		private BufferedReader reader;
+        private BufferedReader reader;
 
-		public CharactersSpliterator(String filename) throws IOException {
-			this.reader = new BufferedReader(new FileReader(filename));
-		}
+        public CharactersSpliterator(String filename) throws IOException {
+            this.reader = new BufferedReader(new FileReader(filename));
+        }
 
-		public Stream<Character> stream() {
-			return StreamSupport.stream(this, false);
-		}
+        public Stream<Character> stream() {
+            return StreamSupport.stream(this, false);
+        }
 
-		@Override
-			public boolean tryAdvance(Consumer<? super Character> action) {
-				try {
-					int charCode = reader.read();
-					if (charCode != -1) {
-						action.accept((char) charCode);
-						return true;
-					} else {
-						return false;
-					}
-				} catch (IOException e) {
-					e.printStackTrace();
-					return false;
-				}
-			}
+        @Override
+        public boolean tryAdvance(Consumer<? super Character> action) {
+            try {
+                int charCode = reader.read();
+                if (charCode != -1) {
+                    action.accept((char) charCode);
+                    return true;
+                } else {
+                    return false;
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+                return false;
+            }
+        }
 
-		@Override
-			public Spliterator<Character> trySplit() {
-				return null;
-			}
+        @Override
+        public Spliterator<Character> trySplit() {
+            return null;
+        }
 
-		@Override
-			public long estimateSize() {
-				return Long.MAX_VALUE;
-			}
+        @Override
+        public long estimateSize() {
+            return Long.MAX_VALUE;
+        }
 
-		@Override
-			public int characteristics() {
-				return Spliterator.NONNULL | Spliterator.ORDERED | Spliterator.SIZED;
-			}
+        @Override
+        public int characteristics() {
+            return Spliterator.NONNULL | Spliterator.ORDERED | Spliterator.SIZED;
+        }
 	}
 
     public static class AllWordsSpliterator implements Spliterator<String> {
@@ -61,8 +61,8 @@ public class test {
         }
 
         public Stream<String> stream() {
-			return StreamSupport.stream(this, false);
-		}
+            return StreamSupport.stream(this, false);
+        }
 
         @Override
         public boolean tryAdvance(Consumer<? super String> action) {
