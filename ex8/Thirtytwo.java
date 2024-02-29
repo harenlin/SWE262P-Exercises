125855e0b6ad727365180c90fa0bcd372db168db
harenlin <allen880530@gmail.com>
2024-02-29 01:16:01 -0800

    x

---
+++ ex8/Thirtytwo.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.regex.Pattern;
+
+
+public class Thirtytwo {
+	public static void main(String[] args){
+		System.out.println("Hello World");
+	}
+}
+
