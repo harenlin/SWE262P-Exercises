72604df98d63c006dd7ceda613bec1b4c8da739a
harenlin <allen880530@gmail.com>
2024-02-29 11:20:19 -0800

    32 done

--- ex8/run.sh
+++ ex8/run.sh
@@ -1,4 +1,9 @@
 javac Thirty.java
 java Thirty ./../pride-and-prejudice.txt > Thirty.txt
 diff Thirty.txt ./../test_out.txt
+
+javac Thirtytwo.java
+java Thirtytwo ./../pride-and-prejudice.txt > Thirtytwo.txt
+diff Thirtytwo.txt ./../test_out.txt
+
 rm *.class
